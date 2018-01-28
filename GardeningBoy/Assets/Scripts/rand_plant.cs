8635f760da54df6cee11224ddd092d1f6c04a9d3
Thomas-David Griedel <s73811@informatik.htw-dresden.de>
2018-01-28 18:42:44 +0100

    Random Growing Plants added

--- GardeningBoy/Assets/Scripts/rand_plant.cs
+++ GardeningBoy/Assets/Scripts/rand_plant.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class rand_plant : MonoBehaviour {
 
-	public int SpriteNr = 3;
+	public int SpriteAnz = 3;
 
 	private int rand = 1;
-
+	private Animator anim;
 	// Use this for initialization
 	void Start () {
-		rand = Random.Range(1,SpriteNr);
-
-
+		rand = Random.Range(1,SpriteAnz);
+		anim = GetComponent<Animator>();
+		anim.SetInteger("rand", rand);
 	}
 	
 	// Update is called once per frame
