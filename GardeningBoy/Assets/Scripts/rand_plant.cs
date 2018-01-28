1e601311d998eac8ffd2c1e0bbe11dd2513c086b
Thomas-David Griedel <s73811@informatik.htw-dresden.de>
2018-01-28 12:17:24 +0100

    improved character animation, first growing plant

---
+++ GardeningBoy/Assets/Scripts/rand_plant.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rand_plant : MonoBehaviour {
+
+	public int SpriteNr = 3;
+
+	private int rand = 1;
+
+	// Use this for initialization
+	void Start () {
+		rand = Random.Range(1,SpriteNr);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
